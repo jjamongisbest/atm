8ce6689b634538b8300e09a5e94b5f90ebea6c52
82109 <82109@jjamong>
2023-03-14 13:05:07 +0900

    Create project ATM

---
+++ atm/src/atm/UserManager.java
@@ -0,0 +1,16 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class UserManager {
+
+	private static ArrayList<User> list = new ArrayList<>();
+
+	// User 에 대한
+
+	// C reate
+	// R ead
+	// U pdate
+	// D elete
+
+}
